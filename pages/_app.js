2d13f07098598ed0776f7b7be5f2834031a2a0fc
Cjewett1 <Clintondjewett>
2023-04-21 22:23:12 -0600

    fixed concert styles being squished mobile

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
     return (
         <>
             <ToastContainer limit={1} />
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
         </>
     );
 }
